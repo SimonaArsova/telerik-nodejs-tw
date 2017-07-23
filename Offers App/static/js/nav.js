7117df6b042ee541159158a23d4a7f8e7621deeb
SimonaArsova <simona.liubomirova@gmail.com>
2017-07-23 03:34:08 +0300

    Fix navigation

--- Offers App/static/js/nav.js
+++ Offers App/static/js/nav.js
@@ -1,8 +1,8 @@
-$(function() {
-    $('.dropdown .dropdown-toggle').on('click', function() {
-        let $this = $(this);
-        $('.dropdown .dropdown-list').css('display', 'none');
-        let $list = $this.next('.dropdown-list');
-        $list.css('display', 'block');
-    });
-});
+// $(function() {
+//     $('.dropdown .dropdown-toggle').on('click', function() {
+//         let $this = $(this);
+//         $('.dropdown .dropdown-list').css('display', 'none');
+//         let $list = $this.next('.dropdown-list');
+//         $list.css('display', 'block');
+//     });
+// });
